ef92f1533f977020e87eb1cb3efb3c165fa1c36c
Ian Santopietro <ian@system76.com>
2018-01-10 14:39:18 -0700

    Fix build problem

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='Easily manage PPAs',
     url='https://github.com/pop_os/repoman',
     license='GNU GPL3',
-    scripts=['repoman'],
+    scripts=['org.pop.repoman'],
     packages=['repoman'],
     data_files=[
         ('share/metainfo', ['data/repoman.appdata.xml']),
